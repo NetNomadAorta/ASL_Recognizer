9b279dfde0a75d88795320e9408db5fb7ef90683
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2023-02-21 01:41:45 -0800

    Create Mouse_Mover.py

---
+++ Mouse_Mover.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+import datetime
+import random
+
+
+date_time = datetime.datetime.now()
+
+print ("Current date and time = %s" % date_time)
+
+print ("Today's date:  = %s/%s/%s" % (date_time.day, date_time.month, date_time.year))
+
+print ("The time is now: = %s:%s:%s" % (date_time.hour, date_time.minute, date_time.second))
+
+while date_time.hour < 7 or date_time.hour > (12+7):
+    # Move pointer relative to current position
+    time.sleep(1)
+    pyautogui.press('volumeup')
+    time.sleep(1)
+    pyautogui.press('volumedown')
+
+    time.sleep(30 + random.randint(0,60))
+
+print("Done and exiting!")
